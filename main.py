0a6cdff6d1527222dedd7278ec99470705cd0b89
Kelsey Goldwein <kmg94@duke.edu>
2025-07-05 10:09:25 -0400

    bot runs hello slash command, all else broken

--- main.py
+++ main.py
@@ -4,27 +4,44 @@ import logging
 from dotenv import load_dotenv
 import os
 
+class Client(commands.Bot):
+    async def on_ready(self):
+        print(f"Ready to puzzle with {self.user.name}")
+
+        try:
+            load_dotenv()
+            guild = discord.Object(id=os.getenv('TEST_SERVER_ID'))
+            synced = await self.tree.sync(guild=GUILD_ID)
+            print(f"Synced commands to guild {guild.id}")
+        except Exception as e:
+                print(f"Error syncing commands: {e}")
+
+
+# pull secret numbers from .env file
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-
+GUILD_ID = discord.Object(id=os.getenv('TEST_SERVER_ID'))
+    
+# setup logger and intents
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+# prefix sorta irrelevant, not used
+client = Client(command_prefix='!', intents=intents)
+
+### END SETUP ###
+
+
 
 puzzle_role = "cruciverbalists"
 
-@bot.event
-async def on_ready():
-    print(f"Ready to puzzle with {bot.user.name}")
+@client.tree.command(name="hello", description="say hello!", guild=GUILD_ID)
+async def sayHello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello!")
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f"Hello {ctx.author.mention}!")
-
-@bot.command()
+@client.tree.command(name="pingme", description="get pinged when a puzzle is posted", guild=GUILD_ID)
 async def pingme(ctx):
     role = discord.utils.get(ctx.guild.roles, name=puzzle_role)
     if role:
@@ -33,7 +50,7 @@ async def pingme(ctx):
     else:
         await ctx.send("Role doesn't exist")
 
-@bot.command()
+@client.tree.command(name="dontpingme", description="stop getting pinged when a puzzle is posted", guild=GUILD_ID)
 async def dontpingme(ctx):
     role = discord.utils.get(ctx.guild.roles, name=puzzle_role)
     if role:
@@ -42,6 +59,12 @@ async def dontpingme(ctx):
     else:
         await ctx.send("Role doesn't exist")
 
+
+
+client.run(token, log_handler=handler, log_level=logging.DEBUG)
+
+
+### OLD CODE STARTS HERE, JUST REFERENCE ###
 # @bot.command()
 # async def dm(ctx, *, msg):
 #     await ctx.author.send(f"You said {msg}")
@@ -66,5 +89,3 @@ async def dontpingme(ctx):
 # async def secret_error(ctx, error):
 #     if isinstance(error, commands.MissingRole):
 #         await ctx.send("You do not have permission to do that!")
-
-bot.run(token, log_handler=handler, log_level=logging.DEBUG)
